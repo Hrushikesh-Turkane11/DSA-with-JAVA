3536245d99a1efec3cbf269b97c41efc68abecc4
HrushikeshTurkane <147384957+Hrushikesh-Turkane11@users.noreply.github.com>
2024-03-05 08:03:05 +0530

    Time: 7 ms (28.13%) | Memory: 44.4 MB (69.20%) - LeetSync

--- DSA/150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ DSA/150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,34 +1,43 @@
 class Solution {
     public int evalRPN(String[] tokens) 
     {   
-        int returnValue = 0;
-        String operators = "+-*/";
- Stack<String> stack = new Stack<String>();
-  for (String t : tokens) {
- if (!operators.contains(t)) { //push to stack if it is a number
- stack.push(t);
- } else {//pop numbers from stack if it is an operator
- int a = Integer.valueOf(stack.pop());
- int b = Integer.valueOf(stack.pop());
- switch (t) {
- case "+":
- stack.push(String.valueOf(a + b));
- break;
- case "-":
- stack.push(String.valueOf(b- a));
- break;
- case "*":
- stack.push(String.valueOf(a * b));
- break;
- case "/":
- stack.push(String.valueOf(b / a));
- break;
- }
- }
- 
- }
- returnValue = Integer.valueOf(stack.pop());
- return returnValue;
- 
+        int result=0;
+        String operators ="+-*/";
+
+        Stack <String> stack=new Stack<String>();
+        for(String t :tokens)
+        {
+            if(!operators.contains(t))
+            {
+                stack.push(t);
+            }
+            else
+            {
+                int a=Integer.valueOf(stack.pop());
+                int b=Integer.valueOf(stack.pop());
+
+                switch(t)
+                {
+                    case "+" :
+                        
+                        stack.push(String.valueOf(a+b));
+                        break;
+                    
+                    case "-":
+                        stack.push(String.valueOf(b-a));
+                        break;
+
+                    case "*":
+                        stack.push(String.valueOf(a*b));
+                        break;
+                    
+                    case "/":
+                        stack.push(String.valueOf(b/a));
+                        break;
+                }
+            }
+        }
+        result=Integer.valueOf(stack.pop());
+        return result;
     }
 }
